c38b1697f01277252bd75f039624c870e20730bb
g1b3r1sh <26889567+g1b3r1sh@users.noreply.github.com>
2021-12-12 00:32:30 -0700

    Add more features to example

--- Main.java
+++ Main.java
@@ -1,8 +1,14 @@
 import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
 import java.math.BigDecimal;
 
@@ -35,7 +41,6 @@ class Main {
 	frame.add(panel);
 
 	Graph graph = new Graph();
-	graph.setMinimumSize(new Dimension(200, 200));
 
 	graph.setLinearPlane(new Range(0, 5), new Range(0, 10));
 	graph.getGraphComponents().add(new Grid(graph, 5, 10));
@@ -46,8 +51,37 @@ class Main {
 	graph.getDataList().getVisualsHandler(data2).plotData().setColor(Color.GREEN);
 	graph.getDataList().getVisualsHandler(data2).connectData().setColor(Color.GREEN);
 
-	panel.add(graph);
+	JScrollPane scrollPane = new JScrollPane(graph);
+	graph.setPreferredSize(new Dimension(1000, 1000));
+	panel.add(scrollPane);
+
+	JPanel bottomAxis = new JPanel();
+	bottomAxis.setLayout(new BoxLayout(bottomAxis, BoxLayout.Y_AXIS));
+	panel.add(bottomAxis, BorderLayout.SOUTH);
+	bottomAxis.add(centeredXLabel("text"));
+	bottomAxis.add(centeredXLabel("foo"));
+
+	JPanel sideAxis = new JPanel();
+	sideAxis.setLayout(new BoxLayout(sideAxis, BoxLayout.Y_AXIS));
+	panel.add(sideAxis, BorderLayout.WEST);
+	//sideAxis.add()
+
+	graph.add(centeredXLabel("foobar"));
 
 	frame.setVisible(true);
+  }
+
+  public static JLabel centeredXLabel(String text)
+  {
+	  JLabel label = new JLabel(text);
+	  label.setAlignmentX(Component.CENTER_ALIGNMENT);
+	  return label;
+  }
+
+  public static JLabel centeredYLabel(String text)
+  {
+	  JLabel label = new JLabel(text);
+	  label.setAlignmentY(Component.CENTER_ALIGNMENT);
+	  return label;
   }
 }
