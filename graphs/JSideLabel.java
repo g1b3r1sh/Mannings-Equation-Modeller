ad80e645535a323c7dccd69101b96c460932fe69
g1b3r1sh <26889567+g1b3r1sh@users.noreply.github.com>
2021-12-12 22:37:39 -0700

    Created class JSideLabel

---
+++ graphs/JSideLabel.java
@@ -0,0 +1,14 @@
+package graphs;
+
+import javax.swing.JLabel;
+import java.awt.Graphics;
+
+// TODO: Finish this class
+public class JSideLabel extends JLabel
+{
+	@Override
+	protected void paintComponent(Graphics g)
+	{
+
+	}
+}
