ff1256d551440008f51ea9219d1d4f438b4c321e
g1b3r1sh <26889567+g1b3r1sh@users.noreply.github.com>
2021-12-16 19:09:30 -0700

    Rename and move data table model

---
+++ graphs/GraphTableModel.java
@@ -0,0 +1,66 @@
+package graphs;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import javax.swing.table.AbstractTableModel;
+
+import hydraulics.Pair;
+
+// TODO: Make editable
+public class GraphTableModel <N extends Number, M extends Number> extends AbstractTableModel
+{
+	private ArrayList<Pair<N, M>> data;
+	private String nameX;
+	private String nameY;
+	
+	public GraphTableModel(DiscreteData<N, M> outsideData, String nameX, String nameY)
+	{
+		super();
+		this.nameX = nameX;
+		this.nameY = nameY;
+		
+		this.populate(outsideData);
+	}
+
+	@Override
+	public int getRowCount()
+	{
+		return this.data.size();
+	}
+
+	@Override
+	public int getColumnCount()
+	{
+		return 2;
+	}
+
+	@Override
+	public Object getValueAt(int rowIndex, int columnIndex)
+	{
+		return columnIndex == 0 ? this.data.get(rowIndex).first : this.data.get(rowIndex).second;
+	}
+	
+	@Override
+	public String getColumnName(int column)
+	{
+		return column == 0 ? this.nameX : this.nameY;
+	}
+
+	@Override
+	public Class<?> getColumnClass(int columnIndex)
+	{
+		return Number.class;
+	}
+	
+	private void populate(DiscreteData<N, M> data)
+	{
+		this.data = new ArrayList<>();
+		Iterator<N> it = data.getXSet().iterator();
+		for (int i = 0; i < data.getXSet().size(); i++)
+		{
+			N x = it.next();
+			this.data.add(new Pair<>(x, data.y(x)));
+		}
+	}
+}
