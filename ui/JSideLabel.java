069050bf00705916f7e78b2b3d4a077e88f253d9
g1b3r1sh <26889567+g1b3r1sh@users.noreply.github.com>
2021-12-16 23:48:21 -0700

    Move JSideLabel from graphs to ui package

---
+++ ui/JSideLabel.java
@@ -0,0 +1,13 @@
+package ui;
+
+import javax.swing.JLabel;
+import java.awt.Graphics;
+
+public class JSideLabel extends JLabel
+{
+	@Override
+	protected void paintComponent(Graphics g)
+	{
+
+	}
+}
