8297a5cffeaac320c9e16eca95800be68cc0e1b6
g1b3r1sh <26889567+g1b3r1sh@users.noreply.github.com>
2022-01-23 23:46:03 -0700

    Create double version of Range

--- graphs/Range.java
+++ graphs/Range.java
@@ -69,4 +69,71 @@ public class Range
 	{
 		return this.getUpper() - this.getLower();
 	}
+
+	public class Double
+	{
+		private double lower;
+		private double upper;
+	
+		public Double(double lower, double upper)
+		{
+			this.lower = lower;
+			this.setUpper(upper);
+		}
+	
+		public Double(Range.Double range)
+		{
+			this(range.getLower(), range.getUpper());
+		}
+	
+		public void copy(Range.Double range)
+		{
+			this.lower = range.lower;
+			this.upper = range.upper;
+		}
+	
+		public double getLower()
+		{
+			return this.lower;
+		}
+	
+		public double getUpper()
+		{
+			return this.upper;
+		}
+	
+		public void setLower(double lower)
+		{
+			if (lower <= this.upper)
+			{
+				this.lower = lower;
+			}
+			else
+			{
+				throw new IllegalArgumentException();
+			}
+		}
+	
+		public void setUpper(double upper)
+		{
+			if (upper >= this.lower)
+			{
+				this.upper = upper;
+			}
+			else
+			{
+				throw new IllegalArgumentException();
+			}
+		}
+		
+		public double getNumber(double fraction)
+		{
+			return this.size() * fraction + this.lower;
+		}
+		
+		public double size()
+		{
+			return this.getUpper() - this.getLower();
+		}
+	}
 }
