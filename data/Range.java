5b01d20d2b53daeae5eaab92c005202779552028
g1b3r1sh <26889567+g1b3r1sh@users.noreply.github.com>
2022-02-05 19:21:19 -0700

    Add ChangeListener functionality to Range

--- data/Range.java
+++ data/Range.java
@@ -1,5 +1,8 @@
 package data;
 
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
 /**
  * Represents numerical range
 **/
@@ -8,6 +11,7 @@ public class Range
 {
 	private int lower;
 	private int upper;
+	private PropertyChangeSupport changeSupport = new PropertyChangeSupport(this);
 
 	public Range(int lower, int upper)
 	{
@@ -17,13 +21,22 @@ public class Range
 
 	public Range(Range range)
 	{
-		this(range.getLower(), range.getUpper());
+		this(range.lower, range.upper);
 	}
 
 	public void copy(Range range)
 	{
-		this.lower = range.lower;
-		this.upper = range.upper;
+		// Must copy so that this is not an invalid range when the first value is copied
+		if (range.getLower() >= this.getUpper())
+		{
+			this.setUpper(range.getUpper());
+			this.setLower(range.getLower());
+		}
+		else
+		{
+			this.setLower(range.getLower());
+			this.setUpper(range.getUpper());
+		}
 	}
 
 	public int getLower()
@@ -40,11 +53,13 @@ public class Range
 	{
 		if (lower < this.upper)
 		{
+			int oldLower = this.lower;
 			this.lower = lower;
+			this.changeSupport.firePropertyChange("lower", oldLower, lower);
 		}
 		else
 		{
-			throw new IllegalArgumentException();
+			throw new IllegalArgumentException("Lower cannot be greater than or equal to upper.");
 		}
 	}
 
@@ -52,88 +67,123 @@ public class Range
 	{
 		if (upper > this.lower)
 		{
+			int oldUpper = this.upper;
 			this.upper = upper;
+			this.changeSupport.firePropertyChange("upper", oldUpper, upper);
 		}
 		else
 		{
-			throw new IllegalArgumentException();
+			throw new IllegalArgumentException("Upper cannot be less than or equal to lower.");
 		}
 	}
-	
+
 	public double getNumber(double fraction)
 	{
 		return this.size() * fraction + this.lower;
 	}
-	
+
 	public int size()
 	{
 		return this.getUpper() - this.getLower();
+	}
+
+	public void addPropertyChangeListener​(PropertyChangeListener listener)
+	{
+		this.changeSupport.addPropertyChangeListener(listener);
+	}
+
+	public void removePropertyChangeListener​(PropertyChangeListener listener)
+	{
+		this.changeSupport.removePropertyChangeListener(listener);
 	}
 
 	public static class Double
 	{
 		private double lower;
 		private double upper;
-	
+		private PropertyChangeSupport changeSupport = new PropertyChangeSupport(this);
+
 		public Double(double lower, double upper)
 		{
 			this.lower = lower;
 			this.setUpper(upper);
 		}
-	
+
 		public Double(Range.Double range)
 		{
-			this(range.getLower(), range.getUpper());
+			this(range.lower, range.upper);
 		}
-	
+
 		public void copy(Range.Double range)
 		{
-			this.lower = range.lower;
-			this.upper = range.upper;
+			if (range.getLower() > this.getUpper())
+			{
+				this.setUpper(range.getUpper());
+				this.setLower(range.getLower());
+			}
+			else
+			{
+				this.setLower(range.getLower());
+				this.setUpper(range.getUpper());
+			}
 		}
-	
+
 		public double getLower()
 		{
 			return this.lower;
 		}
-	
+
 		public double getUpper()
 		{
 			return this.upper;
 		}
-	
+
 		public void setLower(double lower)
 		{
 			if (lower <= this.upper)
 			{
+				double oldLower = this.lower;
 				this.lower = lower;
+				this.changeSupport.firePropertyChange("lower", oldLower, lower);
 			}
 			else
 			{
-				throw new IllegalArgumentException();
+				throw new IllegalArgumentException("Lower cannot be greater than upper.");
 			}
 		}
-	
+
 		public void setUpper(double upper)
 		{
 			if (upper >= this.lower)
 			{
+				double oldUpper = this.upper;
 				this.upper = upper;
+				this.changeSupport.firePropertyChange("upper", oldUpper, upper);
 			}
 			else
 			{
-				throw new IllegalArgumentException();
+				throw new IllegalArgumentException("Upper cannot be less than lower.");
 			}
 		}
-		
+
 		public double getNumber(double fraction)
 		{
 			return this.size() * fraction + this.lower;
 		}
-		
+
 		public double size()
 		{
 			return this.getUpper() - this.getLower();
+		}
+
+		public void addPropertyChangeListener​(PropertyChangeListener listener)
+		{
+			this.changeSupport.addPropertyChangeListener(listener);
+		}
+
+		public void removePropertyChangeListener​(PropertyChangeListener listener)
+		{
+			this.changeSupport.removePropertyChangeListener(listener);
 		}
 	}
 }
